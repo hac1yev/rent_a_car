f2e6c88f8d492f47221306307662ad9cfc29f0c9
elvin <elvincabbarl@gmail.com>
2023-10-04 12:31:59 +0400

    slider

---
+++ src/components/Home/MainSlider/MainSlider.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./main.css";
+import element from "../../../assets/images/main-slider/1.png";
+
+const MainSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  };
+  return (
+    <div className="slider-cont">
+      <Slider {...settings}>
+        <div className="slick-div-cont">
+          <img src={element} alt="" />
+          <p>Avtomobil <span>rahatlığı <br /> səyahətinizi mükəmməlləşdirsin!</span></p>
+        </div>
+        <div className="slick-div-cont">
+          <img src={element} alt="" />
+         <p>Avtomobil <span>rahatlığı <br /> səyahətinizi mükəmməlləşdirsin!</span></p>
+        </div>
+        <div className="slick-div-cont">
+          <img src={element} alt="" />
+         <p>Avtomobil <span>rahatlığı <br /> səyahətinizi mükəmməlləşdirsin!</span></p>
+        </div>
+        <div className="slick-div-cont">
+          <img src={element} alt="" />
+         <p>Avtomobil <span>rahatlığı <br /> səyahətinizi mükəmməlləşdirsin!</span></p>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default MainSlider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // import Filter from '../components/Home/Filter';
 import OtherCars from '../components/Home/OtherCars/OtherCars';
+import MainSlider from '../components/Home/MainSlider/MainSlider';
 
 const Home = () => {
     return (
         <div>
+            <MainSlider />
             {/* <Filter /> */}
             <OtherCars />
         </div>
