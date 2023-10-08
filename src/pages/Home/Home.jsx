839b633d063ad669972f967bd7b22458ab2d1e20
hac1yev <ilkinhaciyev955@gmail.com>
2023-10-08 23:13:48 +0400

    aa

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Filter from '../components/Home/Filter/Filter';
-import OtherCars from '../components/Home/OtherCars/OtherCars';
-import MainSlider from '../components/Home/MainSlider/MainSlider';
-import VipCars from '../components/Home/VipCars/VipCars';
-import Info from '../components/Home/Info/Info';
+import Filter from '../../components/Home/Filter/Filter';
+import OtherCars from '../../components/Home/OtherCars/OtherCars';
+import MainSlider from '../../components/Home/MainSlider/MainSlider';
+import VipCars from '../../components/Home/VipCars/VipCars';
+import Info from '../../components/Home/Info/Info';
 import Services from '../../components/Home/Services/Services';
 import './Home.css'
 
