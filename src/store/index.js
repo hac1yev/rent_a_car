8c7aa195cb302589574972f9ddeb8bcd9b0dd457
hac1yev <ilkinhaciyev955@gmail.com>
2023-11-22 14:51:20 +0400

    aaa

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { stepSlice } from "./step-slice";
 import { rentCarSlice } from "./rent_car-slice";
+import { authSlice } from "./auth-slice";
 
 const store = configureStore({
     reducer: {
         stepReducer: stepSlice.reducer,
-        rentCarReducer: rentCarSlice.reducer
+        rentCarReducer: rentCarSlice.reducer,
+        authReducer: authSlice.reducer
     },
     middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
